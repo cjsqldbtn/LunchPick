9930fc4da38e08483860b1d9c3284e3214b18b19
ITSC <ITSC@ISTC>
2026-08-11 17:00:12 +0900

    test

---
+++ LunchPick/src/main/java/com/myspringboot/board1/LunchPickApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot.board1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchPickApplication.class, args);
+	}
+
+}
---
+++ LunchPick/src/test/java/com/myspringboot/board1/LunchPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringboot.board1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
